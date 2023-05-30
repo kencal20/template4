b90f002a5c2549738f660f61611576cdb601d5af
kencal20 <kenadjei20@gmail.com>
2023-05-30 12:18:38 +0000

    updating scrpit .js  with split js component

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
 import headerComponent from "./components/head/head.js"
+import splitComponent from "./components/split/split.js"
 
+document.getElementById("split").innerHTML = splitComponent()
 document.getElementById("head").innerHTML = headerComponent()
