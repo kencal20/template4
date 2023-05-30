cf6308a834ba281b9188b9e052e8ca7ebd7bb6cd
kencal20 <kenadjei20@gmail.com>
2023-05-30 12:41:12 +0000

    split js update

--- components/split/split.js
+++ components/split/split.js
@@ -4,10 +4,16 @@ export default function splitComponent() {
     <section>
     <div class="split left">
     SPLIT LEFT
+    <p>Lorem ipsum dolor sit amet consectetur,  adipisicing elit. Recusandae officia, necessitatibus culpa aut,
+    vitae consectetur minus vero sit quo corporis ab repellat esse ratione quod nihil dolorem accusamus beatae nisi.
+    </p>
     </div>
     
     <div class="split right">
     SPLIT RIGHT
+    <p>Lorem ipsum dolor sit amet consectetur,  adipisicing elit. Recusandae officia, necessitatibus culpa aut,
+    vitae consectetur minus vero sit quo corporis ab repellat esse ratione quod nihil dolorem accusamus beatae nisi.
+    </p>
     </div>
     </section>
     `
