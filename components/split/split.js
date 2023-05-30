55461b07f4c6ab348e6fa5fc5989e057afd813b1
kencal20 <kenadjei20@gmail.com>
2023-05-30 12:19:29 +0000

    starting with the split component

---
+++ components/split/split.js
@@ -0,0 +1,14 @@
+export default function splitComponent() {
+    return `
+
+    <section>
+    <div class="split left">
+    SPLIT LEFT
+    </div>
+    
+    <div class="split right">
+    SPLIT RIGHT
+    </div>
+    </section>
+    `
+}
