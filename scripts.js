f298ad83c9e9a1d71fb0a142044fec3407161bfa
kencal20 <kenadjei20@gmail.com>
2023-06-03 19:03:54 +0000

    footerComponent implementation in scripts.css

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 import headerComponent from "./components/head/head.js"
 import splitComponent from "./components/split/split.js"
+import footerComponent from "./components/footer/footer.js"
 
 document.getElementById("split").innerHTML = splitComponent()
 document.getElementById("head").innerHTML = headerComponent()
+document.getElementById("footer").innerHTML = footerComponent()
