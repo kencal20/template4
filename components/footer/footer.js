acc5a733902aa298c1cf0c4c0bd60fcf4a6b9508
kencal20 <kenadjei20@gmail.com>
2023-06-03 19:04:26 +0000

    footer update both in js and css

---
+++ components/footer/footer.js
@@ -0,0 +1,12 @@
+export default function footerComponent(){
+    return`
+    <footer>
+    <div class="links">
+    <p> hello </p>
+
+    </div>
+    <p> h1 </p>
+    <p> welcome </p>
+    </footer>
+    `
+}
